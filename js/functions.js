473043de663bf4542e1f68dad08878f9072ac6bd
Surya Wono <suryawono@yahoo.co.id>
2015-01-07 20:30:01 +0700

    change from jquery mobile to onsen-ui

--- js/functions.js
+++ js/functions.js
@@ -4,36 +4,6 @@
  * and open the template in the editor.
  */
 
-$(document).ready(function () {
-    $("#login").on("submit", function (e) {
-        e.preventDefault();
-        $.mobile.loading("show", {
-            text: "Sedang Login",
-            textVisible: true,
-            theme: $.mobile.loader.prototype.options.theme,
-            textonly: false,
-            html: ""
-        });
-        login($("#email").val(), $("#password").val(), function () {
-            $.mobile.loading("hide");
-        });
-
-    })
-})
-
-function login(username, password, callback) {
-    var login = true;
-    if (username == "suryawono@yahoo.co.id" && password == "suhendilau") {
-
-    } else {
-        login = false;
-    }
-    callback();
-    if (!login) {
-        $("#loginTooltip p").html("Email/kata sandi salah");
-        $("#loginTooltip").popup("open");
-    }else{
-        $("#loginTooltip p").html("Login sukses");
-        $("#loginTooltip").popup("open");
-    }
+function login(){
+    
 }
